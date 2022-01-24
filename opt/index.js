5156fe74cab4aee9ba351e499f55ce7e7c0777db
Ivan Svezhenin <ivans200188@gmail.com>
2022-01-24 17:29:19 +0300

    major changes

--- opt/index.js
+++ opt/index.js
@@ -11,31 +11,6 @@ function check() {
   return log.split('\n')[1] !== undefined;
 }
 
-const inter = setInterval(function () {
-  console.log("Checking for ngrok.log")
-  if (check()) {
-    log = fs.readFileSync('ngrok.log', { encoding: 'utf-8' })
-    if (!log.includes('Warning: remote port forwarding failed for listen port')) {
-      if (!log.split('\n')[log.split('\n').length - 1].includes("connect_to localhost port")) {
-        for (i in log) {
-          if (log.split('\n')[i].includes('Forwarding TCP')) {
-            ip = log.split('\n')[i].split(' ')[4]
-            break;
-          }
-        }
-      } else {
-        console.log('Ngrok port retry')
-      }
-    } else {
-
-      ip = 'Ngrok failed, port specified is already taken.'
-    }
-    start()
-    clearInterval(inter)
-  }
-}, 1000)
-
-
 function start() {
   const listener = http.createServer(function (request, response) {
     response.writeHead(200, { 'Content-Type': 'application/json' })
@@ -44,3 +19,31 @@ function start() {
     console.log(`Listening on port: ${listener.address().port}`)
   })
 }
+
+start();
+// const inter = setInterval(function () {
+//   console.log("Checking for ngrok.log")
+//   if (check()) {
+//     log = fs.readFileSync('ngrok.log', { encoding: 'utf-8' })
+//     if (!log.includes('Warning: remote port forwarding failed for listen port')) {
+//       if (!log.split('\n')[log.split('\n').length - 1].includes("connect_to localhost port")) {
+//         for (i in log) {
+//           if (log.split('\n')[i].includes('Forwarding TCP')) {
+//             ip = log.split('\n')[i].split(' ')[4]
+//             break;
+//           }
+//         }
+//       } else {
+//         console.log('Ngrok port retry')
+//       }
+//     } else {
+
+//       ip = 'Ngrok failed, port specified is already taken.'
+//     }
+//     start()
+//     clearInterval(inter)
+//   }
+// }, 1000)
+
+
+
